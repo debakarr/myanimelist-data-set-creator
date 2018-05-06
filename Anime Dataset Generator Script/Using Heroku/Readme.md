9eb3b9768984b3bb7f0b472e278a05270a4a5e08
Debakar Roy <allinonedibakar@gmail.com>
2018-05-06 14:57:52 +0530

    Added files to deploy in Heroku

---
+++ Anime Dataset Generator Script/Using Heroku/Readme.md
@@ -0,0 +1,50 @@
+# How to use Anime Dataset Generator
+
+This script can be used to download anime dataset from [**Myanimelist**](https://myanimelist.net/) using an unofficial MyAnimeList REST API, [**Jikan**](https://jikan.me/docs).
+
+#### Column metadata:
+
+* animeID: id of anime as in anime url [https://myanimelist.net/anime/<span style="color:red">**1**</span>](https://myanimelist.net/anime/1)
+* name: title of anime
+* premiered: premiered on. default format (season year) 
+* genre: list of genre
+* type: type of anime (example TV, Movie etc) 
+* episodes: number of episodes
+* studios: list of studio
+* source: source of anime (example original, manga, game etc) 
+* scored: score of anime
+* scoredBy: number of member scored the anime
+* members: number of member added anime to their list
+
+### Using Heroku
+
+* Before doing this I will request you to watch this video - [Google Sheets and Python](https://youtu.be/vISRn5qFrkM). Here we are using this concept as a base. I just integrated this with heroku.
+
+* First you need to visit [this link](https://console.developers.google.com/cloud-resource-manager) to create a project inside Google cloud resource manager.
+
+* Click **CREATE PROJECT**, then give it a name. [If below gif is low quality, then click here](https://gfycat.com/gifs/detail/VibrantQuarterlyFieldmouse).
+
+
+![](demo/createProject.gif)
+
+* Now you need to enable Google sheet API for your project.
+
+![](demo/enableAPI.gif)
+
+* Next you need to get credential file. [If below gif is low quality, then click here](https://gfycat.com/gifs/detail/InsecureExcellentImpala).
+
+![](demo/createClientScretJSON.gif)
+
+* [Get files for deployment here].
+
+* Add client_secret.json and give access to the spreadsheet. Spreadsheet contains header, which you need to add. [Watch how to do that here](https://youtu.be/M-q0ptxOJB0).
+
+* Before deploying to Heroku. You need to create an app. [If below gif is low quality, then click here](https://gfycat.com/gifs/detail/AggressiveParallelDevilfish).
+
+![](demo/preDeploy.gif)
+
+* At last just push to heroku master and start the worker dyno. [Watch how to do that here](https://youtu.be/BvlCLwEMKHg)
+
+* Final Product:
+
+![](demo/herokuFinal.gif)
---
+++ Anime Dataset Generator Script/Using Heroku/app.py
@@ -0,0 +1,95 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import cfscrape
+from bs4 import BeautifulSoup
+import json
+import sys
+import re
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+requests = cfscrape.create_scraper()
+lastUrl = "https://myanimelist.net/anime.php?em=0&ed=0&ey=0&o=2&c[0]=a&c[1]=d&cv=1&w=0&sd=21&sm=4&sy=2018&o=2&w=1"
+page = requests.get(lastUrl)
+soup = BeautifulSoup(page.content, 'html.parser')
+x = soup.findAll("a", {"class": "hoverinfo_trigger fw-b fl-l"})[0]['href']
+lastAnimeID = re.search(r'anime/([0-9]+)/', x).group(1)
+
+# Paste the data given below in the spreadsheet and the split it into columns
+# animeID, name, premiered, genre, type, episodes, producer, licensor, studio, source, scored, scoredBy, members
+sheet = client.open('animeData').sheet1
+
+last_row = len(sheet.col_values(1))
+print(last_row)
+if(last_row < 2):
+	lastAnimeInsertedInSheet = 0
+else:
+	lastAnimeInsertedInSheet = sheet.cell(last_row, 1).value
+
+if int(lastAnimeInsertedInSheet) == int(lastAnimeID):
+	sys.exit(0)
+
+start = int(lastAnimeInsertedInSheet)
+
+for i in range(start + 1, start + 1000):
+	apiUrl = 'http://api.jikan.me/anime/' + str(i)
+
+	page = requests.get(apiUrl)
+
+	retry = 0
+
+	print('STATUS CODE:', page.status_code)
+
+	while page.status_code == 429:
+		retry = retry + 1
+		print('Limit reached. Too many request. Retrying..')
+		print('Retry count:', retry)
+		page = requests.get(apiUrl)
+
+	c = page.content
+
+	print('Fetching JSON...')
+
+	jsonData = json.loads(c)
+
+	if('error' not in jsonData):
+		l = []
+
+		name = jsonData['title']
+
+		print('Reading', name, 'animelist...')
+
+		studio = []
+		genre = []
+		producer = []
+		licensor = []
+
+		for j in range(0, len(jsonData['licensor'])):
+			licensor.append(jsonData['licensor'][j]['name'])
+
+		for j in range(0, len(jsonData['producer'])):
+			producer.append(jsonData['producer'][j]['name'])
+
+		for j in range(0, len(jsonData['studio'])):
+			studio.append(jsonData['studio'][j]['name'])
+
+		for j in range(0, len(jsonData['genre'])):
+			genre.append(jsonData['genre'][j]['name'])
+
+		l.append(str(i))
+		l.append(str(name))
+		l.append(str(jsonData['premiered']))
+		l.append(str(genre))
+		l.append(str(jsonData['type']))
+		l.append(str(jsonData['episodes']))
+		l.append(str(producer))
+		l.append(str(licensor))
+		l.append(str(studio))
+		l.append(str(jsonData['source']))
+		l.append(str(jsonData['score']))
+		l.append(str(jsonData['scored_by']))
+		l.append(str(jsonData['members']))
+		print(l)
+		sheet.append_row(l, value_input_option='RAW')
